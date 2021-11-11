6edf9b3e9e0b7d6a0a086fdbbfe74509552b567d
Anatoliy Savchenko <26318217+CeasersCipher@users.noreply.github.com>
2021-11-11 07:14:41 -0500

    Add files via upload

---
+++ snakey.py
@@ -0,0 +1,208 @@
+import pygame
+import time
+import random
+from pygame.locals import *
+import os
+
+# Game Initialization
+pygame.init()
+
+# Center the Game Application
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+# Game Fonts
+font = "Retro.ttf"
+
+# Game Resolution
+screen_width=800
+screen_height=600
+screen=pygame.display.set_mode((screen_width, screen_height))
+
+# Text Renderer
+def text_format(message, textFont, textSize, textColor):
+    newFont=pygame.font.Font(textFont, textSize)
+    newText=newFont.render(message, 0, textColor)
+
+    return newText
+
+
+# Colors
+black=(0, 0, 0)
+gray=(50, 50, 50)
+yellow = (245, 224, 66)
+blue=(52,210,235)
+red=(235,52,128)
+green=(52,235,100)
+black=(77,54,49)
+white=(218,243,247)
+
+
+
+
+# Game Framerate
+clock = pygame.time.Clock()
+FPS=30
+
+dis = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Snake by Anatoliy')
+ 
+snake_block = 10
+global snake_speed
+snake_speed = 15
+ 
+font_style = pygame.font.SysFont("bahnschrift", 25)
+score_font = pygame.font.SysFont("chalkboard", 35)
+
+# Main Menu
+def main_menu():
+
+    menu=True
+    selected="start"
+
+    while menu:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_UP:
+                    selected="start"
+                elif event.key==pygame.K_DOWN:
+                    selected="quit"
+                if event.key==pygame.K_RETURN:
+                    if selected=="start":
+                        print("Start")
+                        gameLoop()
+                    if selected=="quit":
+                        pygame.quit()
+
+
+        # Main Menu UI
+        screen.fill(blue)
+        title=text_format("SNAKEY", font, 90, yellow)
+        if selected=="start":
+            text_start=text_format("START", font, 75, white)
+        else:
+            text_start = text_format("START", font, 75, black)
+        if selected=="quit":
+            text_quit=text_format("QUIT", font, 75, white)
+        else:
+            text_quit = text_format("QUIT", font, 75, black)
+
+        title_rect=title.get_rect()
+        start_rect=text_start.get_rect()
+        quit_rect=text_quit.get_rect()
+
+        # Main Menu Text
+        screen.blit(title, (screen_width/2 - (title_rect[2]/2), 80))
+        screen.blit(text_start, (screen_width/2 - (start_rect[2]/2), 300))
+        screen.blit(text_quit, (screen_width/2 - (quit_rect[2]/2), 360))
+        pygame.display.update()
+        clock.tick(FPS)
+        pygame.display.set_caption("Python Snake Game")
+
+
+def Your_score(score):
+    value = score_font.render("   Your Score: " + str(score), True, yellow)
+    dis.blit(value, [0, 0])
+ 
+ 
+ 
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
+ 
+ 
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [screen_width / 6, screen_height / 3])
+ 
+ 
+def gameLoop():
+    game_over = False
+    game_close = False
+ 
+    x1 = screen_width / 2
+    y1 = screen_height / 2
+ 
+    x1_change = 0
+    y1_change = 0
+ 
+    snake_List = []
+    Length_of_snake = 1
+ 
+    foodx = round(random.randrange(0, screen_width - snake_block) / 10.0) * 10.0
+    foody = round(random.randrange(0, screen_height - snake_block) / 10.0) * 10.0
+ 
+    while not game_over:
+ 
+        while game_close == True:
+            dis.fill(blue)
+            message("You Lost! Press 'C' to Play Again, 'M' for Menu, or 'Q' to Quit", red)
+            Your_score(Length_of_snake - 1)
+            pygame.display.update()
+ 
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_m:
+                        main_menu()
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+ 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_block
+                    x1_change = 0
+ 
+        if x1 >= screen_width or x1 < 0 or y1 >= screen_height or y1 < 0:
+            game_close = True
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(blue)
+        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
+        snake_Head = []
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        snake_List.append(snake_Head)
+        if len(snake_List) > Length_of_snake:
+            del snake_List[0]
+ 
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
+ 
+        our_snake(snake_block, snake_List)
+        Your_score(Length_of_snake - 1)
+
+        pygame.display.update()
+ 
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, screen_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, screen_height - snake_block) / 10.0) * 10.0
+            Length_of_snake += 1
+ 
+        clock.tick(snake_speed)
+ 
+    pygame.quit()
+    quit()
+ 
+#Initialize the Game
+main_menu()
+
+pygame.quit()
+quit()
